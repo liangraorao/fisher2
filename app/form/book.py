15cdbcc0fd20b996a87fcfb56e04e12fd29764a2
liangraorao <aiworaorao@163.com>
2018-11-03 23:22:26 +0800

    wtforms

---
+++ app/form/book.py
@@ -0,0 +1,7 @@
+from wtforms import Form, StringField, IntegerField
+from wtforms.validators import Length, NumberRange, DataRequired
+
+
+class SearchForm(Form):
+    q = StringField(validators=[DataRequired(), Length(min=1, max=30, message='长度在1-30个字符之间')])
+    page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
